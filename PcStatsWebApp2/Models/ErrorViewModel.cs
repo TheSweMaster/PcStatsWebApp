790fcf9dda610a3844c8c08e993f47247e8dc6f8
Jacob Gandemo <theswemaster@gmail.com>
2020-10-31 22:11:39 +0100

    Add project base files

---
+++ PcStatsWebApp2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PcStatsWebApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
